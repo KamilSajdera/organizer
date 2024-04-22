60f1c7b6afa460b2c1534eac1b3cd9e4cc083702
Kamil Sajdera <kamilsajdera@gmail.com>
2024-04-22 11:59:47 +0200

    Added server side validation to sign up form

--- components/AuthPage/SignUpForm.js
+++ components/AuthPage/SignUpForm.js
@@ -1,39 +1,48 @@
+import { useFormState } from "react-dom";
+import registerUser from "@/lib/auth";
+
 import styles from "./SignUpForm.module.scss";
 
 import InputPassword from "./InputPassword";
+import ErrorBlock from "./ErrorBlock";
 
 export default function SignUpForm() {
+  const [state, formAction] = useFormState(registerUser, { message: null });
+
   return (
-    <form className={styles.form}>
-      <div className={styles.inputBox}>
-        <input type="text" name="text" id="text" required />
-        <label htmlFor="text">Username</label>
-      </div>
-      <div className={styles.inputBox}>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          required
-          autoComplete="off"
+    <>
+      {state?.message && <ErrorBlock message={state.message} />}
+      <form className={styles.form} action={formAction}>
+        <div className={styles.inputBox}>
+          <input type="text" name="username" id="username" required />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div className={styles.inputBox}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <InputPassword
+          type="password"
+          name="password"
+          styles="signUp"
+          label="Password"
         />
-        <label htmlFor="email">Email</label>
-      </div>
-      <InputPassword
-        type="password"
-        name="password"
-        styles="signUp"
-        label="Password"
-      />
-      <InputPassword
-        type="password"
-        name="Cpassword"
-        styles="signUp"
-        label="Confirm password"
-      />
-      <button type="submit" className={styles.btnLog}>
-        Sign Up
-      </button>
-    </form>
+        <InputPassword
+          type="password"
+          name="Cpassword"
+          styles="signUp"
+          label="Confirm password"
+        />
+        <button type="submit" className={styles.btnLog}>
+          Sign Up
+        </button>
+      </form>
+    </>
   );
 }
