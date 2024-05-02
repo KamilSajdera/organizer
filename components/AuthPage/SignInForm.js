32c04356b915d57a1a6be707c5882672ea04f2a9
Kamil Sajdera <kamilsajdera@gmail.com>
2024-05-02 14:23:57 +0200

    Added backend sign in validation

--- components/AuthPage/SignInForm.js
+++ components/AuthPage/SignInForm.js
@@ -1,34 +1,44 @@
+import { useFormState } from "react-dom";
+
 import styles from "./SignInForm.module.scss";
 
 import InputPassword from "./InputPassword";
+import ErrorBlock from "./ErrorBlock";
+
+import { loginUser } from "@/lib/auth";
 
 export default function SignInForm() {
+  const [state, formAction] = useFormState(loginUser, null);
+
   return (
-    <form className={styles.form}>
-      <div className={styles.inputBox}>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder=""
-          autoComplete="off"
-          required
+    <>
+    {state?.message && <ErrorBlock message={state.message}/>}
+      <form className={styles.form} action={formAction}>
+        <div className={styles.inputBox}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder=""
+            autoComplete="off"
+            required
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <InputPassword
+          type="password"
+          name="password"
+          styles="signIn"
+          label="Password"
         />
-        <label htmlFor="email">Email</label>
-      </div>
-      <InputPassword
-        type="password"
-        name="password"
-        styles="signIn"
-        label="Password"
-      />
-      <div className={styles.inputRemember}>
-        <input type="checkbox" id="remember" />
-        <label htmlFor="remember">Remember me</label>
-      </div>
-      <button type="submit" className={styles.btnLog}>
-        Log in
-      </button>
-    </form>
+        <div className={styles.inputRemember}>
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember">Remember me</label>
+        </div>
+        <button type="submit" className={styles.btnLog}>
+          Log in
+        </button>
+      </form>
+    </>
   );
 }
