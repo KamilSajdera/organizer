8993d478c9d260d6e1bb253ed8f1d3371aa34f6e
Kamil Sajdera <kamilsajdera@gmail.com>
2024-05-09 19:49:26 +0200

    Added confirmation modal during deleting task

---
+++ components/Tasks/DeleteConfirmation.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./DeleteConfirmation.module.scss";
+import { deleteTask } from "@/lib/tasks";
+
+export default function DeleteConfirmation({ taskId, onCloseModal }) {
+  const [isDeleting, setIsDeleting] = useState();
+  async function deleteHandle() {
+    setIsDeleting(true);
+    await deleteTask(taskId);
+    onCloseModal();
+    setIsDeleting(false);
+  }
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <h3>Delete task</h3>
+        <p>
+          Are you sure you want to delete this task? This operation cannot be
+          undone!
+        </p>
+        <button
+          className={`${styles.button} ${styles["button-delete"]}`}
+          style={isDeleting ? { background: "#e77070" } : {}}
+          onClick={deleteHandle}
+        >
+          {isDeleting ? "Deleting" : "Delete"}
+        </button>
+        <button className={styles.button} onClick={onCloseModal}>
+          Back
+        </button>
+      </div>
+    </div>
+  );
+}
--- components/Tasks/TaskManage.js
+++ components/Tasks/TaskManage.js
@@ -1,21 +1,23 @@
 "use client";
+
+import { useEffect, useRef, useState } from "react";
 import styles from "./TaskManage.module.scss";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faEllipsisVertical,
-  faArrowRight,
-  faTrash,
-} from "@fortawesome/free-solid-svg-icons";
-import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
-import { useEffect, useRef } from "react";
-import { updateCategory, deleteTask } from "@/lib/tasks";
-
 import containerStyles from "./TasksContainer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical, faArrowRight, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+
+import { updateCategory } from "@/lib/tasks";
+import DeleteConfirmation from "./DeleteConfirmation";
 
 export default function TaskManage({ id, category }) {
+  const [isUserWantDelete, setIsUserWantDelete] = useState(false);
   const manageBoxRef = useRef();
   let tasksContainer;
+
+  const availableCategory = ["ToDo", "InProgress", "Done"].filter(
+    (item) => item !== category
+  );
 
   useEffect(() => {
     tasksContainer = document.querySelector(
@@ -41,10 +43,6 @@ export default function TaskManage({ id, category }) {
     else manageBoxRef.current.classList.remove(styles.open);
   };
 
-  const availableCategory = ["ToDo", "InProgress", "Done"].filter(
-    (item) => item !== category
-  );
-
   async function changeTaskCategoryHandle(category) {
     tasksContainer.classList.add(containerStyles.taskMoving);
     await updateCategory(id, category);
@@ -54,31 +52,34 @@ export default function TaskManage({ id, category }) {
     }, 500);
   }
 
-  async function deleteTaskHandle() {
-    tasksContainer.classList.add(containerStyles.taskMoving);
-    await deleteTask(id);
-    setTimeout(() => {
-      tasksContainer.classList.remove(containerStyles.taskMoving);
-    }, 500);
-  }
-
   return (
-    <div className={styles["task-item_manage"]} onClick={() => openBoxHandle()}>
-      <FontAwesomeIcon icon={faEllipsisVertical} />
-      <ul className={styles["manage-box"]} ref={manageBoxRef}>
-        <li onClick={() => changeTaskCategoryHandle(availableCategory[0])}>
-          <FontAwesomeIcon icon={faArrowRight} /> {availableCategory[0]}
-        </li>
-        <li onClick={() => changeTaskCategoryHandle(availableCategory[1])}>
-          <FontAwesomeIcon icon={faArrowRight} /> {availableCategory[1]}
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faPenToSquare} /> Edit
-        </li>
-        <li onClick={() => deleteTaskHandle()}>
-          <FontAwesomeIcon icon={faTrash} /> Delete
-        </li>
-      </ul>
-    </div>
+    <>
+      <div
+        className={styles["task-item_manage"]}
+        onClick={() => openBoxHandle()}
+      >
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+        <ul className={styles["manage-box"]} ref={manageBoxRef}>
+          <li onClick={() => changeTaskCategoryHandle(availableCategory[0])}>
+            <FontAwesomeIcon icon={faArrowRight} /> {availableCategory[0]}
+          </li>
+          <li onClick={() => changeTaskCategoryHandle(availableCategory[1])}>
+            <FontAwesomeIcon icon={faArrowRight} /> {availableCategory[1]}
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPenToSquare} /> Edit
+          </li>
+          <li onClick={() => setIsUserWantDelete(true)}>
+            <FontAwesomeIcon icon={faTrash} /> Delete
+          </li>
+        </ul>
+      </div>
+      {isUserWantDelete && (
+        <DeleteConfirmation
+          taskId={id}
+          onCloseModal={() => setIsUserWantDelete(false)}
+        />
+      )}
+    </>
   );
 }
