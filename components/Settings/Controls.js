5a2109d892f637cf689f343f84f95c1fc47dc7cb
Kamil Sajdera <kamilsajdera@gmail.com>
2024-05-24 11:13:15 +0200

    Added logout function

--- components/Settings/Controls.js
+++ components/Settings/Controls.js
@@ -1,10 +1,18 @@
+'use client'
+
 import styles from './Controls.module.scss';
+import { logOut } from '@/lib/auth';
 
 export default function Controls() {
+
+    async function handleLogOut() {
+        await logOut();
+    }
+
     return (
         <div className={styles['user-controls']}>
             <button>Change password</button>
-            <button>Log out</button>
+            <button onClick={handleLogOut}>Log out</button>
         </div>
     )
 }
