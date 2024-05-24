9f13697b5ef4f1d2f92b5b2893bc786c65a167f8
Kamil Sajdera <kamilsajdera@gmail.com>
2024-05-24 13:36:15 +0200

    Added 'ConfirmationArea' UI component

---
+++ ui/ConfirmationArea.js
@@ -0,0 +1,24 @@
+import styles from "./ConfirmationArea.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboardCheck } from "@fortawesome/free-solid-svg-icons";
+
+export default function ConfirmationArea({
+  children,
+  onConfirmation,
+  onClose,
+}) {
+  return (
+    <div className={styles.confirmationOverlay}>
+      <div className={styles.confirmationWrapper}>
+        <FontAwesomeIcon icon={faClipboardCheck} />
+        <h3>Are you sure?</h3>
+        <p>{children}</p>
+        <div className={styles.controls}>
+          <button onClick={onClose}>Cancel</button>
+          <button>Confirm</button>
+        </div>
+      </div>
+    </div>
+  );
+}
