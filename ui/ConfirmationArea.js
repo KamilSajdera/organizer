ea7d482b5cc3e5cfacb0283cf63c5a962f589d62
Kamil Sajdera <kamilsajdera@gmail.com>
2024-05-27 14:12:23 +0200

    Added loading state in ConfirmationArea UI component

--- ui/ConfirmationArea.js
+++ ui/ConfirmationArea.js
@@ -1,13 +1,21 @@
 import styles from "./ConfirmationArea.module.scss";
 
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClipboardCheck } from "@fortawesome/free-solid-svg-icons";
+import { faClipboardCheck, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function ConfirmationArea({
   children,
   onConfirmation,
   onClose,
 }) {
+  const [isPending, setIsPending] = useState(false);
+  function handleOnConfirmation() {
+    onConfirmation();
+    setIsPending(true);
+  }
+
   return (
     <div className={styles.confirmationOverlay}>
       <div className={styles.confirmationWrapper}>
@@ -16,7 +24,10 @@ export default function ConfirmationArea({
         <p>{children}</p>
         <div className={styles.controls}>
           <button onClick={onClose}>Cancel</button>
-          <button onClick={onConfirmation}>Confirm</button>
+          <button onClick={handleOnConfirmation}>
+            Confirm
+            {isPending && <span class={styles.loader}></span>}
+          </button>
         </div>
       </div>
     </div>
