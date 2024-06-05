1722ed547bc5a6acf6109a5002f25f18ca74ddf0
Kamil Sajdera <kamilsajdera@gmail.com>
2024-06-05 12:35:57 +0200

    Other date format

--- components/Tasks/TaskItem.js
+++ components/Tasks/TaskItem.js
@@ -5,15 +5,31 @@ import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 
 import TaskManage from "./TaskManage";
 
-export default function TaskItem({ index, _id, category, title, description, priority, date, add_date }) {
+export default function TaskItem({
+  index,
+  _id,
+  category,
+  title,
+  description,
+  priority,
+  date,
+  add_date,
+}) {
   return (
     <div className={`${styles["task-item"]} ${styles[`task-item-${index}`]}`}>
       <h4>{title}</h4>
       <TaskManage category={category} id={_id.toString()} />
-      <div className={styles["task-item_tiles"]} >
-        <div className={`${styles.tile} ${styles[`${priority}`]}`}>{priority}</div>
+      <div className={styles["task-item_tiles"]}>
+        <div className={`${styles.tile} ${styles[`${priority}`]}`}>
+          {priority}
+        </div>
         <div className={styles.tile}>
-          {date} <FontAwesomeIcon icon={faCheckCircle} />
+          {new Date(date).toLocaleDateString("pl-PL", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })}{" "}
+          <FontAwesomeIcon icon={faCheckCircle} />
         </div>
       </div>
       <p className={styles["task-item_description"]}>{description}</p>
