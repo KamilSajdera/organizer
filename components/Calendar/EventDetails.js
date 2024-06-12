f8889de4cf935550a46f1dc101824ee5047d6eff
Kamil Sajdera <kamilsajdera@gmail.com>
2024-06-12 17:38:07 +0200

    Appropriate display of accurate data

--- components/Calendar/EventDetails.js
+++ components/Calendar/EventDetails.js
@@ -1,17 +1,64 @@
-import styles from './EventDetails.module.scss';
+import styles from "./EventDetails.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-regular-svg-icons';
+const formatDateTime = (date) => {
+  const day = date.toLocaleDateString("pl-PL", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const time = `${hours < 10 ? "0" + hours : hours}:${
+    minutes < 10 ? "0" + minutes : minutes
+  }`;
 
-export default function EventDetails() {
-    return <div className={styles['event-details']}>
-        <FontAwesomeIcon icon={faCircleInfo} />
-        <h3>event1</h3>
-        <p>Say hello to Web Awesome, the biggest and best library of open-source web components.</p>
-        <div className={styles['event-details_duration']}>
-            <FontAwesomeIcon icon={faClock} /> All day
-        </div> 
-        <button className={styles.btnClose}>Close</button>
+  return { day, time };
+};
+
+export default function EventDetails({
+  title,
+  description,
+  all_day,
+  has_end,
+  start,
+  end,
+  onClose,
+}) {
+  const startDateTime = formatDateTime(start);
+  const endDateTime = has_end ? formatDateTime(end) : { day: "", time: "" };
+
+  const isSameDay =
+    start.toLocaleDateString("pl-PL") === end.toLocaleDateString("pl-PL");
+
+  const displayStartData = isSameDay
+    ? startDateTime.time
+    : `<b>${startDateTime.day}</b> ${startDateTime.time}`;
+
+  const displayEndData = has_end
+    ? isSameDay
+      ? endDateTime.time
+      : `<b>${endDateTime.day}</b> ${endDateTime.time}`
+    : "";
+
+  const durationFormat = all_day
+    ? "All day"
+    : `${displayStartData} - ${displayEndData}`;
+
+  return (
+    <div className={styles["event-details"]}>
+      <FontAwesomeIcon icon={faCircleInfo} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <div className={styles["event-details_duration"]}>
+        <FontAwesomeIcon icon={faClock} />{" "}
+        <div dangerouslySetInnerHTML={{ __html: durationFormat }} />
+      </div>
+      <button className={styles.btnClose} onClick={onClose}>
+        Close
+      </button>
     </div>
+  );
 }
