2ffe3250ffa5c409ed3aee2325f18477e04705b1
Kamil Sajdera <kamilsajdera@gmail.com>
2024-07-03 14:01:53 +0200

    Added page title

--- app/settings/page.js
+++ app/settings/page.js
@@ -1,6 +1,11 @@
 
 import Container from "@/components/Settings/Container";
 
+export const metadata = {
+    title: "Settings | Manageo - personal assistant"
+  };
+  
+
 export default async function SettingsPage() {
     return <Container />
 }
