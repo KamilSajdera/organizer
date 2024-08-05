1a1a6e7e37bd78073ad6a885053ab5bd29f29ae6
Kamil Sajdera <kamilsajdera@gmail.com>
2024-08-05 14:38:33 +0200

    Added info about dashboard page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-   <h1>NEXT JS!</h1>
+   <center>
+    <img src="https://cdn-icons-png.flaticon.com/512/8297/8297314.png" width="150px"/>
+    <h3>Dashboard in build...</h3>
+   </center>
   );
 }
