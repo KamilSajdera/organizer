1609229bb2a1d9cd5c0cd6032b78324c5390cc23
Kamil Sajdera <kamilsajdera@gmail.com>
2024-08-05 13:58:11 +0200

    Page active mark added

--- components/Expenses/DetailsPagination.js
+++ components/Expenses/DetailsPagination.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import styles from "./DetailsPagination.module.scss";
 
 export default function DetailsPagination({ expensesAmount, onChangePage }) {
+  const [activePage, setActivePage] = useState(1);
   let howManyPages = Math.ceil(expensesAmount / 6);
 
   let content = [];
 
+  let handleClickPage = (pageNr) => {
+    onChangePage(pageNr);
+    setActivePage(pageNr);
+  };
+
   for (let i = 1; i <= howManyPages; i++) {
     content.push(
       <div
-        className={styles["pagination-item"]}
+        className={`${styles["pagination-item"]} ${
+          activePage === i && styles.active
+        }`}
         key={i}
-        onClick={() => onChangePage(i)}
+        onClick={() => handleClickPage(i)}
       >
         {i}
       </div>
