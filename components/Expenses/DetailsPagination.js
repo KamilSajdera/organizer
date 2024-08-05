ba8b614c32e1a498d32c8b31248205cc24bc8062
Kamil Sajdera <kamilsajdera@gmail.com>
2024-08-05 14:19:54 +0200

    Dynamic generation of pagination range

--- components/Expenses/DetailsPagination.js
+++ components/Expenses/DetailsPagination.js
@@ -3,20 +3,39 @@ import styles from "./DetailsPagination.module.scss";
 
 export default function DetailsPagination({ expensesAmount, onChangePage }) {
   const [activePage, setActivePage] = useState(1);
-  let howManyPages = Math.ceil(expensesAmount / 6);
+  const howManyPages = Math.ceil(expensesAmount / 6);
 
-  let content = [];
+  if (howManyPages <= 1) return;
 
-  let handleClickPage = (pageNr) => {
+  const handleClickPage = (pageNr) => {
     onChangePage(pageNr);
     setActivePage(pageNr);
   };
 
-  for (let i = 1; i <= howManyPages; i++) {
+  const getPaginationRange = () => {
+    const maxPagesToShow = 3;
+    let startPageIndex = Math.max(
+      1,
+      activePage - Math.floor(maxPagesToShow / 2)
+    );
+    let endPageIndex = startPageIndex + maxPagesToShow - 1;
+
+    if (endPageIndex > howManyPages) {
+      endPageIndex = howManyPages;
+      startPageIndex = Math.max(1, endPageIndex - maxPagesToShow + 1);
+    }
+
+    return { startPageIndex, endPageIndex };
+  };
+
+  const { startPageIndex, endPageIndex } = getPaginationRange();
+
+  const content = [];
+  for (let i = startPageIndex; i <= endPageIndex; i++) {
     content.push(
       <div
         className={`${styles["pagination-item"]} ${
-          activePage === i && styles.active
+          activePage === i ? styles.active : ""
         }`}
         key={i}
         onClick={() => handleClickPage(i)}
