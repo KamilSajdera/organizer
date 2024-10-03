406490ee2b551a2c6e7044eacc1f07d4dea6c13a
Kamil Sajdera <kamilsajdera@gmail.com>
2024-10-03 20:18:52 +0200

    Added emojis

--- components/Expenses/goals-wrapper.js
+++ components/Expenses/goals-wrapper.js
@@ -1,6 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./goals-wrapper.module.scss";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFaceSmile,
+  faFaceMeh,
+  faFaceFrown,
+} from "@fortawesome/free-regular-svg-icons";
 
 export default function GoalsWrapper({ goals, onCloseGoals }) {
   function handleClickClose() {
@@ -11,9 +16,27 @@ export default function GoalsWrapper({ goals, onCloseGoals }) {
     <div className={styles.goals}>
       <div className={styles["goals-container"]}>
         <h3>Your goals</h3>
-        <FontAwesomeIcon icon={faClose} onClick={handleClickClose} />
+        <FontAwesomeIcon
+          icon={faClose}
+          className={styles.closeSvg}
+          onClick={handleClickClose}
+        />
         {goals.map((item) => {
           const goalProgress = Math.round((item.collected / item.amount) * 100);
+          const emojiColor =
+            goalProgress <= 30
+              ? "red"
+              : goalProgress > 30 && goalProgress <= 70
+              ? "yellow"
+              : "green";
+
+          const emojiSvg =
+            goalProgress <= 30
+              ? faFaceFrown
+              : goalProgress > 30 && goalProgress <= 70
+              ? faFaceMeh
+              : faFaceSmile;
+
           return (
             <div className={styles["goal-item"]} key={item._id}>
               <h4>{item.title}</h4>
@@ -31,7 +54,14 @@ export default function GoalsWrapper({ goals, onCloseGoals }) {
                   style={{ width: `${goalProgress}%` }}
                 ></div>
               </div>
-              <p className={styles["goal-progress_percent"]}>{goalProgress}%</p>
+              <p className={styles["goal-progress_percent"]}>
+                {goalProgress}%{" "}
+                <FontAwesomeIcon
+                  icon={emojiSvg}
+                  className={styles.emoji}
+                  style={{ color: emojiColor }}
+                />
+              </p>
               <button className={styles["deposit-btn"]}>Deposit</button>
             </div>
           );
