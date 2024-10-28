e896447f16082365f2a2536b003a229649d099b9
Kamil Sajdera <kamilsajdera@gmail.com>
2024-10-28 12:44:12 +0100

    Added error handling

--- components/Settings/ImagePicker.js
+++ components/Settings/ImagePicker.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState, useTransition } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Image from "next/image";
 import styles from "./Container.module.scss";
@@ -13,15 +13,16 @@ import { faCheck, faX } from "@fortawesome/free-solid-svg-icons";
 
 import { uploadImage } from "@/lib/cloudinary";
 import { updateUserData } from "@/lib/settings";
+import ErrorBlock from "../AuthPage/ErrorBlock";
 
 export default function ImagePicker({ userId, userImage }) {
   const inputRef = useRef();
   const [pickedImage, setPickedImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(undefined);
 
   const isUserHasImage = userImage.trim().length > 0;
   const displayingLogo = isUserHasImage ? userImage : default_logo;
-  
 
   const handleButtonClick = () => {
     inputRef.current.click();
@@ -53,59 +54,75 @@ export default function ImagePicker({ userId, userImage }) {
     try {
       const url = await uploadImage(pickedImage);
       let result = await updateUserData(userId, "profile_image", url);
+
+      if (!result.success) {
+        setError(result.errorMessage);
+      }
     } catch (error) {
-      console.error("Upload failed:", error);
+      setError(error.toString());
     } finally {
       setIsLoading(false);
+      setPickedImage(null);
     }
   }
 
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setError(undefined);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, [error]);
+
   return (
-    <div className={styles["image-box"]}>
-      {isLoading && (
-        <div className={styles["lds-ring"]}>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      )}
-      {!isLoading && (
-        <Image
-          src={pickedImage ? pickedImage : displayingLogo}
-          fill
-          alt="User profile image"
+    <>
+      {error && <ErrorBlock message={error} />}
+      <div className={styles["image-box"]}>
+        {isLoading && (
+          <div className={styles["lds-ring"]}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+        {!isLoading && (
+          <Image
+            src={pickedImage ? pickedImage : displayingLogo}
+            fill
+            alt="User profile image"
+          />
+        )}
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          ref={inputRef}
+          onChange={handleImageChange}
         />
-      )}
-      <input
-        type="file"
-        accept="image/png, image/jpeg"
-        ref={inputRef}
-        onChange={handleImageChange}
-      />
-      {pickedImage && (
-        <>
-          <button
-            className={styles["button-accept"]}
-            onClick={handleUploadImage}
-            style={isLoading ? { opacity: "0.5" } : {}}
-          >
-            <FontAwesomeIcon icon={faCheck} />
+        {pickedImage && (
+          <>
+            <button
+              className={styles["button-accept"]}
+              onClick={handleUploadImage}
+              style={isLoading ? { opacity: "0.5" } : {}}
+            >
+              <FontAwesomeIcon icon={faCheck} />
+            </button>
+            <button
+              className={styles["button-cancel"]}
+              onClick={handleCancelUpload}
+              style={isLoading ? { opacity: "0.5" } : {}}
+            >
+              <FontAwesomeIcon icon={faX} />
+            </button>
+          </>
+        )}
+        {!pickedImage && (
+          <button onClick={handleButtonClick}>
+            <FontAwesomeIcon icon={faImage} />
           </button>
-          <button
-            className={styles["button-cancel"]}
-            onClick={handleCancelUpload}
-            style={isLoading ? { opacity: "0.5" } : {}}
-          >
-            <FontAwesomeIcon icon={faX} />
-          </button>
-        </>
-      )}
-      {!pickedImage && (
-        <button onClick={handleButtonClick}>
-          <FontAwesomeIcon icon={faImage} />
-        </button>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
