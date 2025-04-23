901eba3c6afb286b3306cfba143307223f042ef4
Kamil Sajdera <kamilsajdera@gmail.com>
2025-04-23 18:48:25 +0200

    Added next/prev buttons to change range of displaying months

--- components/Dashboard/expenses-cost-chart.js
+++ components/Dashboard/expenses-cost-chart.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AgCharts } from "ag-charts-react";
 import styles from "./expenses-cost-charts.module.scss";
 
@@ -20,91 +20,111 @@ export default function ExpensesCostChart({ expenses }) {
     "Dec",
   ];
 
-  const currentMonth = new Date().getMonth();
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [chartOptions, setChartOptions] = useState({});
 
-  let monthlyCosts = new Array(12).fill(0);
+  useEffect(() => {
+    let monthlyCosts = new Array(12).fill(0);
+    expenses.forEach((expense) => {
+      const monthIndex = new Date(expense.date).getMonth();
+      monthlyCosts[monthIndex] += expense.amount;
+    });
 
-  expenses.forEach((expense) => {
-    const monthIndex = new Date(expense.date).getMonth();
-    monthlyCosts[monthIndex] += expense.amount;
-  });
+    let lastCostsAmount = [];
+    let lastMonthsName = [];
+    for (let i = 5; i >= 0; i--) {
+      const monthIndex = (currentMonth - i + 12) % 12;
+      lastMonthsName.push(monthsName[monthIndex]);
+      lastCostsAmount.push(monthlyCosts[monthIndex]);
+    }
 
-  let lastCostsAmount = [];
-  let lastMonthsName = [];
-  for (let i = 5; i >= 0; i--) {
-    const monthIndex = (currentMonth - i + 12) % 12;
-    lastMonthsName.push(monthsName[monthIndex]);
-    lastCostsAmount.push(monthlyCosts[monthIndex]);
+    setChartOptions({
+      data: lastMonthsName.map((month, index) => ({
+        month,
+        userExpense: lastCostsAmount[index],
+      })),
+      series: [
+        {
+          type: "line",
+          xKey: "month",
+          yKey: "userExpense",
+          label: {
+            formatter: ({ value }) => `${value}$`,
+            color: "#aaa",
+          },
+          marker: {
+            fill: "#9772bb",
+            size: 13,
+            stroke: "#141319",
+            strokeWidth: 3,
+            shape: "diamond",
+          },
+        },
+      ],
+      axes: [
+        {
+          type: "category",
+          position: "bottom",
+          label: {
+            color: "#ccc",
+          },
+          line: {
+            stroke: "#363636",
+          },
+        },
+        {
+          type: "number",
+          gridLine: {
+            style: [
+              { stroke: "#424046" },
+              { stroke: "#565656", lineDash: [5, 5] },
+            ],
+          },
+          position: "left",
+          label: {
+            color: "#ccc",
+            fontSize: 14,
+            fontWeight: "bold",
+            formatter: ({ value }) => `$${value}`,
+          },
+          line: {
+            stroke: "#363636",
+          },
+        },
+      ],
+      background: {
+        fill: "transparent",
+      },
+    });
+  }, [currentMonth, expenses]);
+
+  function handleChangeRange(action) {
+
+    if(action === "prev")
+      setCurrentMonth((month) => (month - 1 + 12) % 12);
+
+    if(action === "next")
+      setCurrentMonth((month) => (month + 1 + 12) % 12);
   }
-
-  const [chartOptions, setChartOptions] = useState({
-    data: lastMonthsName.map((month, index) => ({
-      month,
-      userExpense: lastCostsAmount[index],
-    })),
-    series: [
-      {
-        type: "line",
-        xKey: "month",
-        yKey: "userExpense",
-        label: {
-          formatter: ({ value }) => `${value}$`,
-          color: "#aaa"
-        },
-        marker: {
-          fill: "#9772bb",
-          size: 13,
-          stroke: "#141319",
-          strokeWidth: 3,
-          shape: "diamond",
-        },
-      },
-    ],
-    axes: [
-      {
-        type: "category",
-        position: "bottom",
-        label: {
-          color: "#ccc",
-        },
-        line: {
-          stroke: "#363636",
-        },
-      },
-      {
-        type: "number",
-        gridLine: {
-          style: [
-            {
-              stroke: "#424046",
-            },
-            {
-              stroke: "#565656",
-              lineDash: [5, 5],
-            },
-          ],
-        },
-        position: "left",
-        label: {
-          color: "#ccc",
-          fontSize: 14,
-          fontWeight: "bold",
-          formatter: ({ value }) => `$${value}`,
-        },
-        line: {
-          stroke: "#363636",
-        },
-      },
-    ],
-    background: {
-      fill: "transparent",
-    },
-  });
 
   return (
     <section className={styles.expensesChartContainer}>
       <h3>Goals & Expenses</h3>
-      <AgCharts options={chartOptions} className={styles.chart} />
+      {chartOptions.data && (
+        <AgCharts options={chartOptions} className={styles.chart} />
+      )}
+      <button
+        className={styles.button}
+        onClick={() => handleChangeRange("prev")}
+      >
+        Prev
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => handleChangeRange("next")}
+      >
+        Next
+      </button>
     </section>
   );
 }
